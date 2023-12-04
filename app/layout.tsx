0c0d8a7d3a8742895cdedfc5a7862d0df5459f37
Luis Paredes <lparedesr@fi.uba.ar>
2023-12-04 09:18:16 -0300

    feat: added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Navbar from "./navbar"
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/assets/images/logo.ico" sizes="any" />
       <body>
         <Navbar />
         {children}
--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,14 @@ import { Button } from "components/Button/Button"
 import { LP_GRID_ITEMS } from "lp-items"
 
 export const metadata: Metadata = {
-  title: "Next.js Enterprise Boilerplate",
-  twitter: {
-    card: "summary_large_image",
-  },
+  title: "Panda.corp",
   openGraph: {
     url: "https://next-enterprise.vercel.app/",
     images: [
       {
         width: 1200,
         height: 630,
-        url: "https://raw.githubusercontent.com/Blazity/next-enterprise/main/project-logo.png",
+        url: "/assets/images/logo.png",
       },
     ],
   },
