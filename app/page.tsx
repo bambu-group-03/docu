1d470daa09f981b973d39959bd8c97c08c9fe207
Luis Paredes <lparedesr@fi.uba.ar>
2023-12-08 13:18:14 -0300

    updated front screen

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,10 @@ export default function Web() {
   return (
     <div className="h-screen items-center justify-center bg-white dark:bg-gray-900">
       <section className="bg-white dark:bg-gray-900">
-        <div className="mx-auto grid max-w-screen-xl px-4 py-8 text-center lg:py-16">
+        <div className="mx-auto grid max-w-screen-xl gap-8 px-4 py-8 text-center lg:grid-cols-2 lg:py-16">
+          <div className="flex items-center justify-center">
+            <img src="/assets/images/demo.png" alt="Your Image" className="h-auto max-w-full" />
+          </div>
           <div className="mx-auto place-self-center">
             <h1 className="mb-4 max-w-2xl text-8xl font-extrabold leading-none tracking-tight dark:text-white md:text-5xl xl:text-6xl">
               Snap
@@ -13,12 +16,14 @@ export default function Web() {
             <p className="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">
               Find out what is happening in the world right now
             </p>
-            <Button href="https://github.com/bambu-group-03/snap/releases/tag/v1.1.2" className="mr-3">
-              Get Mobile Apk
-            </Button>
-            <Button href="https://next-app-identity-socializer-luiscusihuaman.cloud.okteto.net/" intent="secondary">
-              Go to Admin Page
-            </Button>
+            <div className="flex flex-col gap-4 lg:flex-row">
+              <Button href="https://github.com/bambu-group-03/snap/releases/tag/v1.1.2" className="mr-3">
+                Get Mobile Apk
+              </Button>
+              <Button href="https://next-app-identity-socializer-luiscusihuaman.cloud.okteto.net/" intent="secondary">
+                Go to Admin Page
+              </Button>
+            </div>
           </div>
         </div>
 
